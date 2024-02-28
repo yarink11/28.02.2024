b6bf2a21d12238a2cceaff4e873a255d454d54e9
fullstack290623 <fullstack290623@gmail.com>
2024-02-28 18:53:08 +0200

    .

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello sqlite3');
+const knex = require('knex')
+
+const data_base = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: 'our_db.db'
+    },
+    useNullAsDefault: true
+})
+
+// GET GET/ID POST PUT DELETE 
+
+async function get_all() {
+    const employees = await data_base.raw("select * from COMPANY")
+    console.log(employees);
+}
+
+get_all()
