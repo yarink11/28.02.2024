b4d57bfe9297053241ca73ca4a324ddbabba2bb0
fullstack290623 <fullstack290623@gmail.com>
2024-02-28 19:08:40 +0200

    .

--- app.js
+++ app.js
@@ -10,9 +10,22 @@ const data_base = knex({
 
 // GET GET/ID POST PUT DELETE 
 
+async function create_table() {
+    await data_base.raw(`CREATE TABLE IF NOT EXISTS COMPANY (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NAME TEXT NOT NULL,
+        AGE INT NOT NULL,
+        ADDRESS CHAR(50),
+        SALARY REAL);`);
+}
+
+// insert_rows()
+
 async function get_all() {
     const employees = await data_base.raw("select * from COMPANY")
     console.log(employees);
 }
 
+create_table()
+insert_rows()
 get_all()
